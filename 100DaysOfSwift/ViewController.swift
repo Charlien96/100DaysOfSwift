736600e78676626da698e35056a040c1c6aa5181
Charles Newman <chaz.new96@gmail.com>
2022-07-08 14:03:39 +0100

    Completed sets section

--- 100DaysOfSwift/ViewController.swift
+++ 100DaysOfSwift/ViewController.swift
@@ -13,12 +13,15 @@ class ViewController: UIViewController {
     let paul = "Paul McCartney"
     let george = "George Harrison"
     let ringo = "Ringo Starr"
-    
+    let colors = Set(["red", "green", "blue"])
+    let colors2 = Set(["red", "green", "blue", "red", "blue"])
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         printArray()
+        print(colors)
+        print(colors2)
     }
 
     func printArray() {
