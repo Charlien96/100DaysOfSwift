0a47e351b11b461727bdc747ef201a1a9231f15e
Charles Newman <chaz.new96@gmail.com>
2022-07-08 10:47:46 +0100

    Completed strings and variables section

--- 100DaysOfSwift/ViewController.swift
+++ 100DaysOfSwift/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var str = "Hello World"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(str)
+        strOverride()
     }
 
+    func strOverride() {
+        str = "Goodbybe"
+        print(str)
+    }
 
 }
 
